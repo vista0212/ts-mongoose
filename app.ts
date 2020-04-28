e979fb9c5776fdc29e64d4621d4bce462e5c5d19
k1oo <kloo21202@gmail.com>
2020-04-29 06:22:41 +0900

    initial commit

---
+++ app.ts
@@ -0,0 +1,25 @@
+import * as bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
+import * as express from 'express';
+import * as morgan from 'morgan';
+
+dotenv.config();
+
+const app: express.Application = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use((req, res, next) => {
+  // err.status = 404;
+  res.json({
+    message: 'error',
+  });
+});
+
+export default app;
